f60db9b739a312aba6de0ea54166639b7b128655
skygupta07 <mailfromakashgupta@gmail.com>
2024-07-13 10:55:30 +0530

    xor queries

---
+++ 76_BitManipulation_xorOperation/6_xorQueriesOfSubarray.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+int main(){
+
+}
+
+class Solution {
+public:
+    vector<int> xorQueries(vector<int>& arr, vector<vector<int>>& queries) {
+        vector<int> ans;
+        
+        for (auto q : queries){
+            int xorr = 0;   // reason zero xor anything is anything...
+            for (int i=q[0]; i<=q[1]; i++){
+                xorr ^= arr[i]; 
+            }
+            ans.push_back(xorr);
+        }
+
+        return ans;
+    }
+};
